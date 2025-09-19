731b7adb1e61687b25f11d91d9515d9d8cb3bbb3
Holden Lucas <8461253+HoldenLucas@users.noreply.github.com>
2025-09-19 14:24:27 -0400

    move readme back

---
+++ README.md
@@ -0,0 +1,6 @@
+# Holden's Tab Limiter
+
+This is a Firefox extension that limits the maximum number of open tabs.
+When the tab limit is reached, opening another tab will put it in a tab group called the Temporary tab group (TTG).
+The TTG will only contain at most one tab.
+If there is already a tab in the TTG, opening another tab will close the existing temporary tab.
--- background.js
+++ background.js
@@ -1,10 +1,3 @@
-// # Holden's Tab Limiter
-//
-// This is a Firefox extension that limits the maximum number of open tabs.
-// When the maximum number of tabs is reached, opening another tab will put it in the Temporary tab group.
-// The Temporary tab group should only ever contain one tab.
-// If there is already a tab in the Temporary group, opening another tab will close the existing temporary tab.
-
 import { TAB_LIMIT_KEY } from "./constants.js";
 
 const TEMPORARY_GROUP_TITLE = "Temporary\u200B"; // Invisible zero-width space
